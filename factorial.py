fd897e7a95c696c86d4f39d8d91580e93d899d9b
Sheroz Gulomaliev <sherozgulomaliev237@gmail.com>
2025-02-10 19:46:22 +0000

    Добавлен метод вычисления факториала для feature/recursive

--- factorial.py
+++ factorial.py
@@ -1,1 +1,15 @@
 
+def factorial(n, method="iterative"):
+    if method == "recursive":
+        if n == 0 or n == 1:
+            return 1
+        else:
+            return n * factorial(n - 1, method="recursive")
+    else:
+        # Placeholder for iterative method
+        pass
+
+if __name__ == "__main__":
+    number = 5
+    method = input("Choose method (iterative/recursive): ").strip().lower()
+    print(f"Factorial of {number} is {factorial(number, method)}")
